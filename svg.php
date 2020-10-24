6fb270e1eaeb50bdf595b129b8166e29382777e8
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-10-24 20:23:15 +0800

    Update svg.php

--- svg.php
+++ svg.php
@@ -8,15 +8,21 @@
                 <div class="c1">
                     <a target="blank" href="svg/SVG1.php">
                         <div class="ctn">
-                            <div><h2>LE 5.1</h2></a></div>
+                            <div><h2>LT 5.1</h2></a></div>
                             <div>SVG Graphical Art</div>
+                        </div>
+                    </a>
+                    <a target="blank" href="svg/SVG2.php">
+                        <div class="ctn">
+                            <div><h2>LT 6.2</h2></a></div>
+                            <div>SVG Animation</div>
                         </div>
                     </a>
                 </div>
                 <div class="c2">
-                    <a target="blank" href="https://codepen.io/angelacoronel/pen/BazLbab">
+                    <a target="blank" href="svg/SVG2-1.php">
                         <div class="ctn">
-                            <div><h2>LT 2.3 Element 1</h2></a></div>
+                            <div><h2>LT 6.1 Element 1</h2></a></div>
                             <div>SVG Text Art</div>
                         </div>
                     </a>
